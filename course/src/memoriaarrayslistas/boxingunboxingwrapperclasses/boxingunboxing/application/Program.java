3c07a1988bbea4a68da7a4d2e90e238a6ae4e58c
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-01-18 21:55:48 -0300

    Adicionado comentarios e implementado unboxing usando tipo primitivo

--- course/src/memoriaarrayslistas/boxingunboxingwrapperclasses/boxingunboxing/application/Program.java
+++ course/src/memoriaarrayslistas/boxingunboxingwrapperclasses/boxingunboxing/application/Program.java
@@ -5,13 +5,25 @@ public class Program {
 	public static void main(String[] args) {
 		
 		int x = 20;
-		Integer obj = x;
+		Object obj1 = x;
+		Integer obj2 = x;
 		
-		System.out.println(obj);
 		
-		int y = obj * 2;
+		System.out.println(obj1);
+		
+		// Object (Tipo primitivo)
+		
+		int y = (int) obj1;
 		
 		System.out.println(y);
+		
+		// Integer (Wrapper Class)
+		
+		y = obj2 * 2;
+		
+		System.out.println(y);
+		
+		
 
 	}
 
